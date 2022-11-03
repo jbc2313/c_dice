fde87ea29a40895e016c949bb3f53834fb1aad0c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-03 12:36:49 -0500

    basic program setup

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num_dice;
+    printf("Lets shoot dice!\n");
+    printf("How many are you throwing?\n");
+    scanf("%d", &num_dice);
+    printf("You grabbed %d dice", num_dice);
+
+
+
+
+
+
+}
