5fa313479594e81e5986cb0444d87b04a42bbd05
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-03 19:12:35 -0500

    added total to output

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h> // sleep function from here
 
+# define randnum(min, max) \
+    ((rand() % (int)(((max) + 1) - (min))) + (min))
+
 int main() {
+    srand(time(NULL));
     int num_dice;
     printf("Lets shoot dice!\n");
     printf("How many are you throwing?\n");
     scanf("%d", &num_dice);
     printf("You grabbed %d dice\n", num_dice);
-    int die1 = 2;
-    int die2 = 6;
+    int die1 = randnum(1,6);
+    int die2 = randnum(1,6);
+    int total = die1 + die2;
     sleep(1);
     printf("Shaking the dice now..\n");
     sleep(3);
-    printf("Die #1 = %d\nDie #2 = %d", die1, die2);
+    printf("Die#1 = %d\nDie#2 = %d\nTotal=%d\n", die1, die2, total);
 
 
 
