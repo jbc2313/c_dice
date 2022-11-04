772b4f3339dacbebd9cdd339ced4f4a15956461b
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-03 19:02:03 -0500

    added pause between choosing dice and throwing dice

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // sleep function from here
 
 int main() {
     int num_dice;
     printf("Lets shoot dice!\n");
     printf("How many are you throwing?\n");
     scanf("%d", &num_dice);
-    printf("You grabbed %d dice", num_dice);
+    printf("You grabbed %d dice\n", num_dice);
+    int die1 = 2;
+    int die2 = 6;
+    sleep(1);
+    printf("Shaking the dice now..\n");
+    sleep(3);
+    printf("Die #1 = %d\nDie #2 = %d", die1, die2);
+
 
 
 
